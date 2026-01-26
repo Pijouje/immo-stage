b4553f3632a09cdcc9c6aeecde36d6c574008aae
Pijouje <millansims4@gmail.com>
2026-01-26 13:26:16 +0100

    Initialisation du projet avec Nuxt

---
+++ app/app.vue
@@ -0,0 +1,38 @@
+<script setup>
+// Pas d'import de CSS ici !
+</script>
+
+<template>
+  <div class="app-container">
+    <header>
+      <h1>🏠 Mon Agence Immo</h1>
+    </header>
+    <main>
+      <p>Le serveur fonctionne enfin ! On peut commencer le Front-End.</p>
+    </main>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #f4f4f9;
+}
+
+.app-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  background-color: white;
+  min-height: 100vh;
+  box-shadow: 0 0 20px rgba(0,0,0,0.05);
+}
+
+header {
+  background-color: #2c3e50;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// nuxt.config.ts
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  // Module SEO pour Google et les IA
+  modules: ['@nuxtjs/seo'],
+
+  // Infos du site
+  site: {
+    url: 'https://ton-site-stage.com',
+    name: 'Agence Immo',
+    description: 'Location appartement étudiant et jeune actif.',
+    defaultLocale: 'fr',
+  },
+
+
+})
