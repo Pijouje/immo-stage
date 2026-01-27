d3e53d7927ba9e4854cb7aa0a65aba3654f48715
alexis1546 <pvaldemaralexis@gmail.com>
2026-01-27 11:39:26 +0100

    rien de special

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
     <NuxtPage />
-=======
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
->>>>>>> cd6a94a668392a13dff562c31fde96e1ef85f66d
   </div>
 </template>
