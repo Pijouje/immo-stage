14ad04d944c2f1247fe46b0593f01cd9a50d258d
Pijouje <millansims4@gmail.com>
2026-01-27 10:58:01 +0100

    Première définition des layouts

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout-container">
+    <TheNavbar />
+
+    <main class="page-content">
+      <slot />
+    </main>
+
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped>
+.layout-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Prend au moins toute la hauteur de l'écran */
+}
+
+.page-content {
+  flex: 1; /* Pousse le footer vers le bas si la page est vide */
+  padding-top: 20px; /* Un peu d'espace sous le menu */
+  background-color: #f9f9f9; /* Fond gris très clair pour les pages standards */
+}
+</style>
