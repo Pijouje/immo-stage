583c2e80ffc88622fdb59ea5d3c8bdda0d7df524
Pijouje <millansims4@gmail.com>
2026-01-28 10:51:47 +0100

    Ajout de layout par defaut

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Navbar /> <main>
+      <slot /> </main>
+  </div>
+</template>
+
+<style>
+
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+@keyframes fadeUp { 
+    from { 
+        opacity: 0; 
+        transform: translateX(10px);
+    } 
+    to { 
+        opacity: 1; 
+        transform: translateX(0); 
+    } 
+}
+
+.auth-enter-active {
+  animation: fadeUp 0.3s ease-out;
+}
+
+.auth-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.auth-leave-to {
+  opacity: 0;
+
+}</style>
