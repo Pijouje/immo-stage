cfc81179791115a60d2fb853a0660fdf1166d43f
alexis1546 <pvaldemaralexis@gmail.com>
2026-01-28 11:40:49 +0100

    debut messagerie

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,56 @@
+<script>
+import { ref } from 'vue'
+
+const nom = ref('')
+const prenom = ref('')
+</script>
+
 <template>
-  <div>
-    <h1>Page contact</h1>
+  <div class="page">
+    <div class="carte">
+      <div class="partie-gauche">
+        <h2>Mes Messages</h2>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.page {
+  min-height: calc(100vh - 90px);
+    width: 100%;
+    background-image: url('/bg.png');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+/* CARTE CENTRALE */
+.carte {
+  background-color: white;
+    width: 100%;
+    max-width: 1200px; 
+    height: 600px;
+    padding: 60px;
+    border-radius: 16px;
+    box-shadow: 0 15px 35px rgba(0,0,0,0.1);
+    display: flex;
+    flex-direction: column;
+    margin: 80px auto; 
+    box-sizing: border-box;
+    transform: translateY(-27px);
+}
+
+.carte h2 {
+  display: flex;
+  justify-content: left;
+  transform: translateY(-40px);
+}
+
+.partie-gauche {
+  width: 320px;
+  height: 100%;
+  border-right: 1px solid #e2e8f0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,7 @@
     <h1>Page d'accueil</h1>
     <p>Le site fonctionne !</p>
     <NuxtLink to="/connexion">Aller vers Connexion</NuxtLink>
+    <br />
+    <NuxtLink to="/contact">Aller vers CONTACT</NuxtLink>
   </div>
 </template>
