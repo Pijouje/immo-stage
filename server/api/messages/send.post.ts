8f292eeff78ea103d1c2762d34da23fd5569d0ab
Pijouje <millansims4@gmail.com>
2026-02-10 11:12:38 +0100

    integration de singleton dans l'api message

--- server/api/messages/send.post.ts
+++ server/api/messages/send.post.ts
@@ -1,7 +1,6 @@
 import { getServerSession } from '#auth'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '../../utils/prisma'
 
-const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
     const session = await getServerSession(event)
